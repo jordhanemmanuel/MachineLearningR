2bcbeacabc05ea078e1ae29de6abbbc500025ef3
jordhanemmanuel <19559411+jordhanemmanuel@users.noreply.github.com>
2024-07-11 22:49:59 -0300

    finished classification and regression

--- t22-classificacao.R
+++ t22-classificacao.R
@@ -9,6 +9,11 @@ veiculos$a <- NULL
 diabetes <- read.csv("10 - Diabetes - Dados.csv")
 diabetes$num <- NULL
 
+veiculos_novos <- read.csv("6 - Veiculos - Novos Casos.csv")
+veiculos_novos$a <- NULL
+diabetes_novos <- read.csv("10 - Diabetes - Novos Casos.csv")
+diabetes_novos$num <- NULL
+
 print_division <- function() {
   print("---------------------------------")
 }
@@ -31,17 +36,17 @@ predizer_resultado <- function(treinado, teste, dataname, tipo_modelo) {
 }
 
 classificacao_knn <- function(treino, teste, formula, dataname) {
-  # Grid com valores de K
   set.seed(202462)
-  tune_grid <- expand.grid(k = (1:10))
-  knn <- train(formula, data = treino, method = "knn", tuneGrid = tune_grid)
+  knn <- train(formula, data = treino, method = "knn")
   predizer_resultado(knn, teste, dataname, "KNN")
+  return(knn)
 }
 
 classificacao_rna_holdout <- function(treino, teste, formula, dataname) {
   set.seed(202462)
   rna <- train(formula, data = treino, method = "nnet", trace = FALSE)
   predizer_resultado(rna, teste, dataname, "RNA Hold-out")
+  return(rna)
 }
 
 classificacao_rna_cv <- function(treino, teste, formula, dataname) {
@@ -50,9 +55,42 @@ classificacao_rna_cv <- function(treino, teste, formula, dataname) {
   rna <- train(formula, data = treino, method = "nnet",
                trace = FALSE, trControl = ctrl)
   predizer_resultado(rna, teste, dataname, "RNA Cross Validation")
+  return(rna)
 }
 
-classificar_todos <- function(data, target_col, dataname) {
+classificacao_svm_holdout <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
+  svm <- train(formula, data = treino, method = "svmRadial")
+  predizer_resultado(svm, teste, dataname, "SVM Hold-out")
+  return(svm)
+}
+
+classificacao_svm_cv <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
+  ctrl <- trainControl(method = "cv", number = 10)
+  svm <- train(formula, data = treino, method = "svmRadial",
+               trControl = ctrl)
+  predizer_resultado(svm, teste, dataname, "SVM Cross Validation")
+  return(svm)
+}
+
+classificacao_rf_holdout <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
+  tune_grid <- expand.grid(mtry = (1:10))
+  rf <- train(formula, data = treino, method = "rf", tuneGrid = tune_grid)
+  predizer_resultado(rf, teste, dataname, "Random Forest Hold-out")
+  return(rf)
+}
+
+classificacao_rf_cv <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
+  ctrl <- trainControl(method = "cv", number = 10)
+  rf <- train(formula, data = treino, method = "rf", trControl = ctrl)
+  predizer_resultado(rf, teste, dataname, "Random Forest Cross Validation")
+  return(rf)
+}
+
+classificar_todos <- function(data, target_col, dataname, newcases) {
   # Divisao 80:20
   set.seed(202462)
   indices <- createDataPartition(data[, ncol(data)], p=0.80, list=FALSE)# nolint
@@ -63,13 +101,38 @@ classificar_todos <- function(data, target_col, dataname) {
   formula <- reformulate(".", response = target_col)
 
   print(paste("KNN ", dataname))
-  classificacao_knn(treino, teste, formula, dataname)
+  knn <- classificacao_knn(treino, teste, formula, dataname)
   print(paste("RNA Hold-out ", dataname))
-  classificacao_rna_holdout(treino, teste, formula, dataname)
+  rna_ho <- classificacao_rna_holdout(treino, teste, formula, dataname)
   print(paste("RNA Cross-validation ", dataname))
-  classificacao_rna_cv(treino, teste, formula, dataname)
+  rna_cv <- classificacao_rna_cv(treino, teste, formula, dataname)
+  print(paste("SVM Hold-out ", dataname))
+  svm_ho <- classificacao_svm_holdout(treino, teste, formula, dataname)
+  print(paste("SVM Cross-validation ", dataname))
+  svm_cv <- classificacao_svm_cv(treino, teste, formula, dataname)
+  print(paste("Random Forest Hold-out ", dataname))
+  rf_ho <- classificacao_rf_holdout(treino, teste, formula, dataname)
+  print(paste("Random Forest Cross-validation ", dataname))
+  rf_cv <- classificacao_rf_cv(treino, teste, formula, dataname)
   print_division()
+
+  # Se 1, é veiculo entao usar SVM - Holdout, 2 é diabetes RF - Holdout
+  if (newcases == 1) {
+    predict_novo <- predict(svm_ho, veiculos_novos)
+    resultado <- cbind(veiculos_novos, predict_novo)
+    return(resultado)
+  } else {
+    predict_novo <- predict(rf_ho, diabetes_novos)
+    resultado <- cbind(diabetes_novos, predict_novo)
+    return(resultado)
+  }
 }
 
-classificar_todos(veiculos, "tipo", "Veiculos")
-classificar_todos(diabetes, "diabetes", "Diabetes")
+# O último parâmetro serve só para controlar qual modelo vai prever
+# os novos dados
+res_veiculos <- classificar_todos(veiculos, "tipo", "Veiculos", 1)
+res_diabetes <- classificar_todos(diabetes, "diabetes", "Diabetes", 2)
+
+print("PREDIÇÃO DE NOVOS CASOS:")
+print(res_veiculos)
+print(res_diabetes)
--- t22-regressao.R
+++ t22-regressao.R
@@ -11,6 +11,10 @@ admissao <- read.csv("9 - Admissao - Dados.csv")
 admissao$num <- NULL
 biomassa <- read.csv("5 - Biomassa - Dados.csv")
 
+admissao_novos <- read.csv("9 - Admissao - Novos Casos.csv")
+admissao_novos$num <- NULL
+biomassa_novos <- read.csv("5 - Biomassa - Novos Casos.csv")
+
 print_division <- function() {
   print("---------------------------------")
 }
@@ -40,7 +44,7 @@ avaliacao <- function(observed, pred) {
   print("Syx:")
   print(val_syx)
   print_division()
-  print("Perason:")
+  print("Pearson:")
   print(val_pearson)
   print_division()
   print("RMSE:")
@@ -51,7 +55,7 @@ avaliacao <- function(observed, pred) {
   print_division()
 }
 
-predizer_resultado <- function(treinado, teste, dataname, tipo_modelo) {
+predizer_resultado <- function(treinado, teste, dataname, tipo_modelo, p = 0) {
   print_division()
   print(paste(tipo_modelo, ":"))
   print(treinado)
@@ -59,30 +63,85 @@ predizer_resultado <- function(treinado, teste, dataname, tipo_modelo) {
   predict_treino <- predict(treinado, teste)
   observado <- teste[, ncol(teste)]
   avaliacao(observado, predict_treino)
+  if (p == 1) {
+    residuals <- observado - predict_treino
+    plot(predict_treino, residuals, main="Admissao em SVM Cross Validation",
+         xlab="Preditos", ylab="Residuais")
+    abline(h=0, col="red")
+  }
+  if (p == 2) {
+    residuals <- observado - predict_treino
+    plot(predict_treino, residuals, main="Biomassa em RF Hold-out",
+         xlab="Preditos", ylab="Residuais")
+    abline(h=0, col="red")
+  }
 }
 
 
 regressao_knn <- function(treino, teste, formula, dataname) {
-  # Grid com valores de K
-  tune_grid <- expand.grid(k = (1:10))
-  knn <- train(formula, data = treino, method = "knn", tuneGrid = tune_grid)
+  set.seed(202462)
+  knn <- train(formula, data = treino, method = "knn")
   predizer_resultado(knn, teste, dataname, "KNN")
+  return(knn)
 }
 
 regressao_rna_holdout <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
   rna <- train(formula, data = treino, method = "nnet",
                linout = TRUE, trace = FALSE)
   predizer_resultado(rna, teste, dataname, "RNA Hold-out")
+  return(rna)
 }
 
 regressao_rna_cv <- function(treino, teste, formula, dataname) {
-  ctrl <- trainControl(method = "cv", number = 10, seeds = 202462)
+  set.seed(202462)
+  ctrl <- trainControl(method = "cv", number = 10)
   rna <- train(formula, data = treino, method = "nnet",
                linout = TRUE, trace = FALSE, trControl = ctrl)
   predizer_resultado(rna, teste, dataname, "RNA Cross Validation")
+  return(rna)
 }
 
-regressao_todos <- function(data, target_col, dataname) {
+regressao_svm_holdout <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
+  svm <- train(formula, data = treino, method = "svmRadial")
+  predizer_resultado(svm, teste, dataname, "SVM Hold-out")
+  return(svm)
+}
+
+regressao_svm_cv <- function(treino, teste, formula, dataname, melhor) {
+  set.seed(202462)
+  ctrl <- trainControl(method = "cv", number = 10)
+  svm <- train(formula, data = treino, method = "svmRadial", trControl = ctrl)
+  if (melhor == 1) {
+    predizer_resultado(svm, teste, dataname, "SVM Cross Validation", melhor)
+  } else {
+    predizer_resultado(svm, teste, dataname, "SVM Cross Validation")
+  }
+  return(svm)
+}
+
+regressao_rf_holdout <- function(treino, teste, formula, dataname, melhor) {
+  set.seed(202462)
+  rf <- train(formula, data = treino, method = "rf")
+  if (melhor == 2) {
+    predizer_resultado(rf, teste, dataname, "Random Forest Hold-out", melhor)
+  } else {
+    predizer_resultado(rf, teste, dataname, "Random Forest Hold-out")
+  }
+  
+  return(rf)
+}
+
+regressao_rf_cv <- function(treino, teste, formula, dataname) {
+  set.seed(202462)
+  ctrl <- trainControl(method = "cv", number = 10)
+  rf <- train(formula, data = treino, method = "rf", trControl = ctrl)
+  predizer_resultado(rf, teste, dataname, "Random Forest Cross Validation")
+  return(rf)
+}
+
+regressao_todos <- function(data, target_col, dataname, melhor) {
   # Divisao 80:20
   indices <- createDataPartition(data[, ncol(data)], p=0.80, list=FALSE)# nolint
   treino <- data[indices, ]
@@ -92,11 +151,38 @@ regressao_todos <- function(data, target_col, dataname) {
   formula <- reformulate(".", response = target_col)
 
   print(paste("KNN ", dataname))
-  regressao_knn(treino, teste, formula, dataname)
+  knn <- regressao_knn(treino, teste, formula, dataname)
+  print(paste("RNA Hold-out ", dataname))
+  rna_ho <- regressao_rna_holdout(treino, teste, formula, dataname)
+  print(paste("RNA Cross-validation ", dataname))
+  rna_cv <- regressao_rna_cv(treino, teste, formula, dataname)
+  print(paste("SVM Hold-out ", dataname))
+  svm_ho <- regressao_svm_holdout(treino, teste, formula, dataname)
+  print(paste("SVM Cross-validation ", dataname))
+  svm_cv <- regressao_svm_cv(treino, teste, formula, dataname, melhor)
+  print(paste("Random Forest Hold-out ", dataname))
+  rf_ho <- regressao_rf_holdout(treino, teste, formula, dataname, melhor)
+  print(paste("Random Forest Cross-validation ", dataname))
+  rf_cv <- regressao_rf_cv(treino, teste, formula, dataname)
   print_division()
+
+  # Se 1, é Admissao entao usar SVM - CV, 2 é Biomassa RF - Holdout
+  if (melhor == 1) {
+    predict_novo <- predict(svm_cv, admissao_novos)
+    resultado <- cbind(admissao_novos, predict_novo)
+    return(resultado)
+  } else {
+    predict_novo <- predict(rf_ho, biomassa_novos)
+    resultado <- cbind(biomassa_novos, predict_novo)
+    return(resultado)
+  }
 }
 
 print("KNN ADMISSAO")
-regressao_todos(admissao, "ChanceOfAdmit", "Admissao")
+res_admissao <- regressao_todos(admissao, "ChanceOfAdmit", "Admissao", 1)
 print("KNN BIOMASSA:")
-regressao_todos(biomassa, "biomassa", "Biomassa")
+res_biomassa <- regressao_todos(biomassa, "biomassa", "Biomassa", 2)
+
+print("PREDIÇÃO DE NOVOS CASOS:")
+print(res_admissao)
+print(res_biomassa)
